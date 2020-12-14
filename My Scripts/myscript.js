ab7e5f3f6ea9b1fffba511ba4c4794fc3e53d3b7
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-12-14 14:45:58 +0100

    my scripts done

--- My Scripts/myscript.js
+++ My Scripts/myscript.js
@@ -1,4 +1,6 @@
 const projects = document.querySelectorAll('.project');
+const tableRows = document.querySelectorAll('.afiliate__box__launch table tbody tr');
+const checkAll = document.querySelector('#check-all');
 
 // Add active links
 const showOptionMenu = (project, evt) => {
@@ -7,6 +9,23 @@ const showOptionMenu = (project, evt) => {
     }
 }
 
+const showTableRowOptions = (tableRow, evt) => {
+    if(evt.target.className === "options-img"){
+        tableRow.querySelector('.options').classList.toggle('none');
+    }
+}
+
 projects.forEach((project) => {
     project.addEventListener('click', (evt) => showOptionMenu(project, evt));
 });
+
+tableRows.forEach((tableRow) => {
+    tableRow.addEventListener('click', (evt) => showTableRowOptions(tableRow, evt));
+});
+
+checkAll.addEventListener('click', () => {
+    checkAll.checked = !checkAll.checked;
+    document.querySelectorAll('.inp-checkboxx').forEach(checkBox => {
+        checkBox.checked = !checkBox.checked;
+    })
+})
