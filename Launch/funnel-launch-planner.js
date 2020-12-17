2a06d97746fae1fcc0ac6263d491834ef7a96f79
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-12-17 01:27:39 +0100

    funnel launch done...

--- Launch/funnel-launch-planner.js
+++ Launch/funnel-launch-planner.js
@@ -1,4 +1,7 @@
 const funnelLinks = document.querySelectorAll('.navigation ul li');
+const funnels = document.querySelectorAll('.funnel');
+const generalOptions = document.querySelector('.title__left .toggle__option__menu')
+const body = document.querySelector('body');
 
 // Add active links
 const funnelLinkActive = (id) => {
@@ -13,6 +16,55 @@ const funnelLinkActive = (id) => {
     }
 }
 
+// CheckAll for funnels and nested elements
+const checkAll = (funnel, evt) => {
+    console.log(evt.target.tagName);
+    let funnelOptions = funnel.querySelector('.funnel__top .options');
+    // Check all
+    if(evt.target.classList.contains('check-all')){
+        let isChecked = !evt.target.checked;
+        evt.target.checked = isChecked;
+        funnel.querySelectorAll('input').forEach(input => {
+            input.checked = !isChecked;
+        })
+    } else if(evt.target.classList.contains('check-all-nest')){ 
+        // for all nested elements
+        let nestedIsChecked = !evt.target.checked;
+        evt.target.checked = nestedIsChecked;
+        const targetRow = [...funnel.querySelectorAll('.funnel__row')].filter(funnelRow => {
+            return funnelRow.contains(evt.target);
+        });
+
+        targetRow[0].querySelectorAll('input').forEach(input => {
+            input.checked = !nestedIsChecked;
+        })
+    } else if(evt.target.tagName === 'IMG'){
+        funnelOptions.classList.toggle('none');
+    } else {
+        !funnelOptions.classList.contains('none') ? funnelOptions.classList.add('none') : null;
+    }
+}
+
+// Event listeners for generalOptions
+generalOptions.addEventListener('click', () => {
+    document.querySelector('.title__left .options').classList.toggle('none');
+})
+
+// Event listener for active links
 funnelLinks.forEach((funnelLink, idx) => {
     funnelLink.addEventListener('click', () => funnelLinkActive(idx));
-})
+});
+
+// Event listeners for checkboxes
+funnels.forEach(funnel => {
+    funnel.addEventListener('click', (evt) => checkAll(funnel, evt));
+});
+
+// close any option if open
+body.addEventListener('click', (evt) => {
+    if(evt.target.className !== 'toggle__option__menu'){
+        document.querySelectorAll('.options').forEach(option => {
+            !option.classList.contains('none') ? option.classList.add('none') : null;
+        })
+    }
+});
--- My Scripts/myscript.js
+++ My Scripts/myscript.js
@@ -1,6 +1,8 @@
+const myProjects = document.querySelector('.my__projects');
 const projects = document.querySelectorAll('.project');
 const tableRows = document.querySelectorAll('.afiliate__box__launch table tbody tr');
 const checkAll = document.querySelector('#check-all');
+const body = document.querySelector('body');
 
 // Add active links
 const showOptionMenu = (project, evt) => {
@@ -23,9 +25,29 @@ tableRows.forEach((tableRow) => {
     tableRow.addEventListener('click', (evt) => showTableRowOptions(tableRow, evt));
 });
 
+if(myProjects){
+    myProjects.addEventListener('click', (evt) => {
+        if (body.contains(evt.target) && evt.target.className !== 'menu'){
+            document.querySelectorAll('.options').forEach(option => {
+                !option.classList.contains('none') ? option.classList.add('none') : null;
+            })
+        }
+    });
+}
+
 checkAll.addEventListener('click', () => {
-    checkAll.checked = !checkAll.checked;
+    let isChecked = !checkAll.checked;
+    checkAll.checked = isChecked;
     document.querySelectorAll('.inp-checkboxx').forEach(checkBox => {
-        checkBox.checked = !checkBox.checked;
+        checkBox.checked = !isChecked;
     })
-})
+});
+
+// close any option if open
+body.addEventListener('click', (evt) => {
+    if(body.contains(evt.target) && evt.target.className !== 'options-img'){
+        document.querySelectorAll('.options').forEach(option => {
+            !option.classList.contains('none') ? option.classList.add('none') : null;
+        })
+    };
+});
